e89c19b66ef917c721066c3a193c1f06636afccc
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-23 21:27:12 +0530

    Add files via upload

---
+++ Behavioural_Visitor_Pattern/src/com/prathyusha/model/ItemElement.java
@@ -0,0 +1,7 @@
+package com.prathyusha.model;
+
+public interface ItemElement {
+	
+	public int accept(ShoppingCartVisitor visitor);
+
+}
---
+++ Behavioural_Visitor_Pattern/src/com/prathyusha/model/ShoppingCartVisitor.java
@@ -0,0 +1,8 @@
+package com.prathyusha.model;
+
+public interface ShoppingCartVisitor {
+	
+	int visit(Book book);
+	int visit(Fruit fruit);
+
+}
